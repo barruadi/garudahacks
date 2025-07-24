4ff3699547ce7e12056e0397658d4c6d214fd109
azfaradhi <aradhihakim@gmail.com>
2025-07-24 16:23:22 +0700

    feat: adding config

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from 'hono'
+import { handle } from 'hono/aws-lambda'
+import { serve } from '@hono/node-server'
+import { cors } from 'hono/cors'
+import { usersRoute } from './routes/users.route'
+
+const app = new Hono()
+
+app.use('*', cors({
+  origin: '*', 
+  credentials: true,
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+}))
+
+app.get('/', (c) => {
+  return c.json({
+    success: true,
+    message: 'Hello Hono with Database!',
+    timestamp: new Date().toISOString(),
+  })
+})
+
+app.route('/users', usersRoute)
+
+
+serve({
+  fetch: app.fetch,
+  port: 8001,
+})
+
+console.log('Server is running on http://localhost:8001')
