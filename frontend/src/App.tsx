0a016420baf25baaac23b7d7db35b66428f25a1e
Barru Adi <barru.adi@gmail.com>
2025-07-25 00:16:26 +0700

    feat: auth fe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,20 @@ import { Routes, Route } from "react-router-dom"
 import './App.css'
 
 import SignUp from "./page/signUp"
-import SignIn from "./page/singIn"
+import SignIn from "./page/signIn"
 import IndonesiaMap from "./page/map"
+import SplashPage from "./page/splash"
+import ListsPage from "./page/lists"
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/login" element={<SignIn />} />
-      <Route path="/register" element={<SignUp />} />
+      <Route path="/" element={<SplashPage />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/map" element={<IndonesiaMap />} />
+      <Route path="/lists" element={<ListsPage />} />
     </Routes>
   )
 }
