3599560ee07746b982a8e42c15103bdf76bda450
rafifrs <rafiffarras023@gmail.com>
2025-07-25 01:57:04 +0700

    feat: add-sites page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 import SignUp from "./page/signUp"
 import SignIn from "./page/singIn"
 import IndonesiaMap from "./page/map"
+import AddSitesPage from "./page/addPageSites"
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Route path="/login" element={<SignIn />} />
       <Route path="/register" element={<SignUp />} />
       <Route path="/map" element={<IndonesiaMap />} />
+      <Route path="/add-sites" element={<AddSitesPage />} />
     </Routes>
   )
 }
